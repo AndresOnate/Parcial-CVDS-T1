241eef1105c2a72e6fc1511627daddf0edb54230
Andres Oñate <admin@oficinaImpri.ecidomo.escuelaing.edu.co>
2023-03-04 09:28:51 -0500

    Implemetacion

--- Parcial/src/main/java/cvds/tdd/FizzBuzz/FizzBuzz.java
+++ Parcial/src/main/java/cvds/tdd/FizzBuzz/FizzBuzz.java
@@ -1,7 +1,53 @@
 package cvds.tdd.FizzBuzz;
 
-import java.util.ArrayList;
-
 public class FizzBuzz {
     
+
+    public String validateInput(int number){
+
+        boolean multipleOfthree = isMultipleOfThree(number);
+        boolean multipleOfFive = isMultipleOfFive(number);
+        boolean containsThree = validateThreeInNumber(number);
+        boolean containsFive = validateFiveInNumber(number);
+
+        if ( (multipleOfFive && multipleOfthree) || (containsFive && containsThree)){
+            return "FizzBuzz";
+        } else if (multipleOfthree || containsThree){
+            return "Fizz";
+        }else if (multipleOfFive|| containsFive){
+            return "Buzz";
+        }
+        return "";
+    }
+
+    public boolean isMultipleOfThree(int number){
+        return number % 3 == 0;
+    }
+
+    public boolean isMultipleOfFive(int number){
+        return number % 5 == 0;
+    }
+
+    public boolean validateThreeInNumber(int number){
+        int slice = 0;
+        while (number > 0) {
+            slice = number % 10;
+            if (slice == 3) {
+              return true;
+            }
+            number = number / 10;
+          }
+        return false;
+    }
+
+    public boolean validateFiveInNumber(int number){
+        while (number > 0) {
+            int slice = number % 10;
+            if (slice == 5) {
+              return true;
+            }
+            number = number / 10;
+          }
+        return false;
+    }
 }
--- Parcial/src/test/java/cvds/tdd/FizzBuzz/FizzBuzzTest.java
+++ Parcial/src/test/java/cvds/tdd/FizzBuzz/FizzBuzzTest.java
@@ -11,10 +11,33 @@ public class FizzBuzzTest {
     public void should_ReturnFizz_When_MultipleOfThreeOnly() {
         
         //Arrange
-        
+        int input = 9;
+        String expectedOutput = "Fizz";
+
         //Action
+
+        String result = fizzBuzz.validateInput(input);
         
         //Assertion
+
+        Assert.assertEquals(expectedOutput, result);
+        
+    }
+
+    @Test
+    public void should_ReturnFizz_When_NumberContainsThreeOnly() {
+        
+        //Arrange
+        int input = 31;
+        String expectedOutput = "Fizz";
+
+        //Action
+
+        String result = fizzBuzz.validateInput(input);
+        
+        //Assertion
+
+        Assert.assertEquals(expectedOutput, result);
         
     }
 
@@ -22,10 +45,33 @@ public class FizzBuzzTest {
     public void should_ReturnBuzz_When_MultipleOFiveOnly() {
         
         //Arrange
-        
+        int input = 10;
+        String expectedOutput = "Buzz";
+
         //Action
+
+        String result = fizzBuzz.validateInput(input);
         
         //Assertion
+
+        Assert.assertEquals(expectedOutput, result);
+        
+    }
+
+    @Test
+    public void should_ReturnBuzz_When_NumberContainsOFiveOnly() {
+        
+        //Arrange
+        int input = 52;
+        String expectedOutput = "Buzz";
+
+        //Action
+
+        String result = fizzBuzz.validateInput(input);
+        
+        //Assertion
+
+        Assert.assertEquals(expectedOutput, result);
         
     }
 
@@ -33,10 +79,33 @@ public class FizzBuzzTest {
     public void should_ReturnFizzBuzz_When_MultipleOFiveandThree() {
         
         //Arrange
-        
+        int input = 15;
+        String expectedOutput = "FizzBuzz";
+
         //Action
+
+        String result = fizzBuzz.validateInput(input);
         
         //Assertion
+
+        Assert.assertEquals(expectedOutput, result);
+        
+    }
+
+    @Test
+    public void should_ReturnFizzBuzz_When_ContainsFiveandThree() {
+        
+        //Arrange
+        int input = 35;
+        String expectedOutput = "FizzBuzz";
+
+        //Action
+
+        String result = fizzBuzz.validateInput(input);
+        
+        //Assertion
+
+        Assert.assertEquals(expectedOutput, result);
         
     }
     
